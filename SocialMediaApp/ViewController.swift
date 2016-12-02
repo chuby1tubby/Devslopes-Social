270df7cc3a82ec769bcaead3125340ece1bc637e
chuby1tubby <knakamura13@apu.edu>
2016-12-02 15:27:50 -0800

    Removing errors

---
+++ SocialMediaApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SocialMediaApp
+//
+//  Created by Kyle Nakamura on 12/2/16.
+//  Copyright © 2016 Kyle Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
