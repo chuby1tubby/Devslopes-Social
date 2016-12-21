238ddadaa78c6a767df58925e1632a4e7b11924a
chuby1tubby <knakamura13@apu.edu>
2016-12-21 14:03:11 -0800

    Image caching and retrieval from Firebase implemented.

--- SocialMediaApp/DataService.swift
+++ SocialMediaApp/DataService.swift
@@ -10,15 +10,20 @@ import Foundation
 import Firebase
 
 let DB_BASE = FIRDatabase.database().reference()
+let ST_BASE = FIRStorage.storage().reference()
 
 class DataService {
     
     // Global object
     static let ds = DataService()
     
+    // DB References
     private var _REF_BASE = DB_BASE
     private var _REF_POSTS = DB_BASE.child("posts")
     private var _REF_USERS = DB_BASE.child("users")
+    
+    // ST References
+    private var _REF_POST_IMAGES = ST_BASE.child("post-pics")
     
     var REF_BASE: FIRDatabaseReference {
          return _REF_BASE
@@ -30,6 +35,10 @@ class DataService {
     
     var REF_USERS: FIRDatabaseReference {
         return _REF_USERS
+    }
+    
+    var REF_POST_IMAGES: FIRStorageReference {
+        return _REF_POST_IMAGES
     }
     
     func createFirebaseDBUser(uid: String, userData: Dictionary <String, String>) {
--- SocialMediaApp/PostCell.swift
+++ SocialMediaApp/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
     
@@ -25,10 +26,32 @@ class PostCell: UITableViewCell {
         // Initialization code
     }
     
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage? = nil) {
         self.post = post
         self.caption.text = post.caption
         self.likesLabel.text = String(post.likes)
+        
+        // Retreive image from device cache OR from Firebase Storage
+        // If image already cached
+        if img != nil {
+            self.postImg.image = img
+        }
+        // Else, download image from URL
+        else {
+            let ref = FIRStorage.storage().reference(forURL: post.imageUrl)
+            ref.data(withMaxSize: 2*1024*1024, completion: { (data, error) in
+                if error != nil {
+                    print("KYLE: Unable to download image from Firebase storage")
+                } else {
+                    print("KYLE: Image downloaded from Firebase storage")
+                    if let imgData = data {
+                        if let img = UIImage(data: imgData) {
+                            self.postImg.image = img
+                            FeedVC.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                        }
+                    }
+                }
+            })
+        }
     }
-    
 }
